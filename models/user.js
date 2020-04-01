33f191af0377e40b530d85e3d2aa9907e857b1fd
JafferAbbas-codes <beastincarnate121@gmail.com>
2020-04-01 16:58:19 +0500

    1/4/2020

--- models/user.js
+++ models/user.js
@@ -1,6 +1,7 @@
 const mongoose = require("mongoose");
 const uuidv1 = require("uuid/v1");
 const crypto = require("crypto");
+const { ObjectId } = mongoose.Schema;
 
 const userSchema = new mongoose.Schema({
   name: {
@@ -22,7 +23,17 @@ const userSchema = new mongoose.Schema({
     type: Date,
     default: Date.now
   },
-  updated: Date
+  updated: Date,
+  photo: {
+    data: Buffer,
+    contentType: String
+  },
+  about: {
+    type: String,
+    trim: true
+  },
+  following: [{ type: ObjectId, ref: "User" }],
+  followers: [{ type: ObjectId, ref: "User" }]
 });
 
 // virtual field
--- routes/user.js
+++ routes/user.js
@@ -4,17 +4,26 @@ const {
   allUsers,
   getUser,
   updateUser,
-  deleteUser
+  deleteUser,
+  userPhoto,
+  addFollowing,
+  addFollower,
+  removeFollowing,
+  removeFollower
 } = require("../controllers/user");
 const { requireSignin } = require("../controllers/auth");
 
 const router = express.Router();
 
+router.put("/user/follow", requireSignin, addFollowing, addFollower);
+router.put("/user/follow", requireSignin, removeFollowing, removeFollower);
+
 router.get("/users", allUsers);
 router.get("/user/:userId", requireSignin, getUser);
 router.put("/user/:userId", requireSignin, updateUser);
 router.delete("/user/:userId", requireSignin, deleteUser);
-
+//photo
+router.get("/user/photo/:userId", userPhoto);
 // any route containing :userId, our app will first execure userById()
 router.param("userId", userById);
 
