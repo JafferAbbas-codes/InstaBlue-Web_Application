db7027f7ebaa6aedd255e233c50f97c801d68687
JafferAbbas-codes <beastincarnate121@gmail.com>
2020-04-28 16:17:35 +0500

    29-April-2020

--- routes/auth.js
+++ routes/auth.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const { signup, signin, signout } = require("../controllers/auth");
+const { signup, signin, signout , socialLogin} = require("../controllers/auth");
 const { userSignValidator } = require("../validator");
 const { userById } = require("../controllers/user");
 
@@ -8,6 +8,8 @@ const router = express.Router();
 router.post("/signup", userSignValidator, signup);
 router.post("/signin", signin);
 router.get("/signout", signout);
+
+router.post("/social-login", socialLogin);
 
 // any route containing :userId, our app will first execure userById()
 router.param("userId", userById);
